d460a70a8bba5b891441d1ccd0fb5bae84a0bb28
areebasaira36-bit <areebasaira36@gmail.com>
2025-10-20 13:06:15 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x;
+    int y;
+    cout << "enter the value of b:";
+    cin >> x;
+if (x == 20) 
+    {
+        y = 0;
+    }
+    else
+    {
+        y = 10;
+    }
+    cout << "the value of y is:" << y << endl;
+    return 0;
+}
---
+++ task1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x;
+    int y;
+    cout << "x=";
+    cin >> x;
+    cout << "y=";
+    cin >> y;
+    if (x > y) 
+    {
+        cout << "x is greater than y" << endl;
+    }
+    else if (x < y)
+    {
+        cout << "y is greater than x" << endl;
+    }
+    else
+    {
+        cout << "x is equal to y" << endl;
+    }
+    return 0;
+}
